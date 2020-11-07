94f1dc910035fdbfb3bd3c03bf60e2c7bb3459c2
Dvir Bartov <dvir.bartov1@gmail.com>
2020-11-07 17:11:21 +0200

    Initial commit, setting up basic express app with mongoDB database

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import config from 'config';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors())
+
+// Create and start connection
+const port: number = parseInt(<string>process.env.PORT) || 5000;
+app.listen(port, () => console.log('Server running'));
+
+// Set database
+const db: string = config.get('mongoURI')
+
+// Connect to MongoDB
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err))
