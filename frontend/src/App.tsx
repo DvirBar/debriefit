ff4d43c90807a71b270ad0cb1b17174ba67c86cc
Dvir Bartov <dvir.bartov1@gmail.com>
2020-11-09 04:21:44 +0200

    Init react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/css/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
